29bb64a96a2d32abc6ee359536bd71d7d27294c1
Simon Farshid <simon@assistant-ui.com>
2025-11-13 18:23:58 -0800

    chore: update deps (#2728)

--- packages/react/src/primitives/assistantModal/scope.tsx
+++ packages/react/src/primitives/assistantModal/scope.tsx
@@ -1,5 +1,7 @@
 import * as PopoverPrimitive from "@radix-ui/react-popover";
 import type { Scope } from "@radix-ui/react-context";
 
-export const usePopoverScope = PopoverPrimitive.createPopoverScope();
+export const usePopoverScope: ReturnType<
+  typeof PopoverPrimitive.createPopoverScope
+> = PopoverPrimitive.createPopoverScope();
 export type ScopedProps<P> = P & { __scopeAssistantModal?: Scope };
